90052d6ebadd9aab50e7dea2890f15cea4adb015
hertelendymm <hertelendymartonm@gmail.com>
2024-09-01 16:51:17 +0200

    Flashcards Core Mechanic + bug fixing

---
+++ lib/widgets/main_app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainAppBar extends StatefulWidget {
+  const MainAppBar({super.key});
+
+  @override
+  State<MainAppBar> createState() => _MainAppBarState();
+}
+
+class _MainAppBarState extends State<MainAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
